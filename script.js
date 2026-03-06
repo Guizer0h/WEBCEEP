be8b7905f45cccaa3769787adf052a4ed34622c2
Guizer0h <hara.guilherme@escola.pr.gov.br>
2026-03-05 21:25:26 -0300

    Implement hamburger menu toggle functionality
    Added functionality for hamburger menu toggle.

--- script.js
+++ script.js
@@ -22,3 +22,10 @@ window.addEventListener('scroll', () => {
     }
 
 });
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
